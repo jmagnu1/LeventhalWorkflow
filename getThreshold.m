0c3e01a8b84ba27ae1918c22a7624434cc02f3ed
Vibin Parakkattu <vibinp@umich.edu>
2015-08-08 10:53:51 -0400

    created amplitude spike detection w/ moving noise window

---
+++ getThreshold.m
@@ -0,0 +1,12 @@
+function threshold = getThreshold(threshGain,data)
+%% sliding window
+window = 1000; %samples
+dt = 1000;
+threshold = zeros(1,length(data));
+for i=window+1:dt:length(data)-window
+   threshold(i:i+dt-1) = threshGain .* median((abs(data(i-window:i+window))./0.6745));
+end
+
+%set threshold values outside the sliding window
+threshold(1:window) = threshold(window+1);
+threshold(end-window:end) = threshold(end-window-1);
