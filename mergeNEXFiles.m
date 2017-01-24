0eb232304bd6f9dc025b74a89d7926cf2c7fa575
dleventh <dleventh@umich.edu>
2017-01-24 14:55:15 -0500

    merge Nex and stuff

--- mergeNEXFiles.m
+++
@@ -1,8 +0,0 @@
-function mergeNEXFiles(sessionConf)
-
-leventhalPaths = buildLeventhalPaths(sessionConf);
-disp('Making box.nex file...');
-TDTtoNex(sessionName,'nasPath',nasPath);
-disp('Merging all .nex files...');
-parts = strsplit(leventhalPaths.processed,filesep);
-combineSessionNex_wf(fullfile(parts{1:end-1}));
