ba2bda2dd517f5c417471a5270fbf5b9c9e1d5d9
Matt Gaidica <matt@gaidi.ca>
2017-04-24 15:34:36 -0400

    readme

---
+++ exportAnalysisConfv3.m
@@ -0,0 +1,45 @@
+function analysisConf = exportAnalysisConfv2(subjects,nasPath)
+
+analysisConf = struct;
+analysisConf.subjects = subjects;
+analysisConf.nasPath = nasPath;
+analysisConf.sessionConfs = {};
+
+allNeurons = {};
+allSessionNames = {};
+allSessionConfs = {};
+neuronCount = 1;
+    
+for iSubjects = 1:numel(subjects)
+    subjects__name = subjects{iSubjects};
+    % get all data folders that exist
+    % use dir2 to remove {'.','..','.DS_Store','._.DS_Store'}
+    dataDirs = dir2(fullfile(nasPath,subjects__name,[subjects__name,'-processed']));
+    for iDataDir=1:length(dataDirs)
+        if ~dataDirs(iDataDir).isdir
+            continue;
+        end
+        % requires network
+        sessionConf = exportSessionConfv2(dataDirs(iDataDir).name,'nasPath',nasPath);
+        if isempty(sessionConf.leventhalPaths.nex) % nex files uncompilled
+            continue;
+        end
+        leventhalPaths = buildLeventhalPathsv2(sessionConf);
+        [nvar, names, types] = nex_info(leventhalPaths.nex);
+        neuronNames = cellstr(deblank(names(types==0,:)));
+        allNeurons = {allNeurons{:} neuronNames{:}};
+        for ii=1:length(neuronNames)
+            allSessionNames = [allSessionNames;sessionConf.sessions__name];
+            allSessionConfs{neuronCount,1} = sessionConf;
+            neuronCount = neuronCount + 1;
+        end
+    end
+    allNeurons = allNeurons';
+    neuronIds = listdlg('PromptString','Select neurons:',...
+                    'SelectionMode','multiple','ListSize',[500 700],...
+                    'ListString',allNeurons);
+
+    analysisConf.neurons = allNeurons(neuronIds);
+    analysisConf.sessionNames = allSessionNames(neuronIds);
+    analysisConf.sessionConfs = allSessionConfs(neuronIds);
+end
--- readme.m
+++ readme.m
@@ -1,3 +1,5 @@
+insert into sessions (`subject_id`,`name`,`date`,`time`,`behavior_id`,`ephys_system_id`,`chamber_id`,`comment`) values(142,'R0142_20161218a','2016/12/18','00:00:00',3,2,2,'');
+
 nasPath = '/Volumes/RecordingsLeventhal2/ChoiceTask';
 sessionConf = exportSessionConfv2('R0142_20161201a','nasPath',nasPath);
 nexData = TDTtoNex(sessionConf);
