0fdcd2d6d4518c976b458bfabe9f2fc5e4f92300
Matt Gaidica <matt@gaidi.ca>
2017-04-27 14:27:57 -0400

    check data line numbers

--- check_nexData.m
+++ check_nexData.m
@@ -2,6 +2,6 @@ function check_nexData(nexData)
 
 disp(char(repmat(46,1,20)));
 for ii=1:length(nexData.events)
-    disp([nexData.events{ii}.name,', ',num2str(length(nexData.events{ii}.timestamps)),' events']);
+    disp([num2str(ii),': ',nexData.events{ii}.name,', ',num2str(length(nexData.events{ii}.timestamps)),' events']);
 end
 disp(char(repmat(46,1,20)));
--- readme.m
+++ readme.m
@@ -1,7 +1,9 @@
 insert into sessions (`subject_id`,`name`,`date`,`time`,`behavior_id`,`ephys_system_id`,`chamber_id`,`comment`) values(142,'R0142_20161218a','2016/12/18','00:00:00',3,2,2,'');
 
-nasPath = '/Volumes/RecordingsLeventhal2/ChoiceTask';
-sessionConf = exportSessionConfv2('R0142_20161201a','nasPath',nasPath);
+subjects__name = 'R0154';
+% nasPath = '/Volumes/RecordingsLeventhal2/ChoiceTask';
+nasPath = '\\172.20.138.142\RecordingsLeventhal2\ChoiceTask';
+sessionConf = exportSessionConfv2([subjects__name,'_20170228a'],'nasPath',nasPath);
 nexData = TDTtoNex(sessionConf);
-processedPath = '\\172.20.138.142\RecordingsLeventhal2\ChoiceTask\R0117\R0117-processed';
+processedPath = fullfile(nasPath,subjects__name,[subjects__name,'-processed']);
 combineSessionNex_wf(processedPath);
