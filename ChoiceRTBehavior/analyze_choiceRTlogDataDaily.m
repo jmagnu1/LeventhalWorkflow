772e0a21fe2b1b482d529b56646e3ff1d5674af8
Matt Gaidica <matt@gaidi.ca>
2017-05-01 14:33:35 -0400

    fix export_fig error

--- ChoiceRTBehavior/analyze_choiceRTlogDataDaily.m
+++ ChoiceRTBehavior/analyze_choiceRTlogDataDaily.m
@@ -412,7 +412,7 @@ for iDir=1:numFolders
 %                 'BackgroundColor','white','fontsize',fontTitle);
         cd(graphFolder);
         figure(h_fig);
-        export_fig(graphName,'-pdf','-q101','-painters','-nocrop');
+        export_fig(graphName,'-pdf','-q101','-nocrop');
 %         saveas(gcf, textString{1}, 'pdf')
         close(h_fig)
         cd(parentFolder);
